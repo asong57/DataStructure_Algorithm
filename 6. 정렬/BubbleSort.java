a5f3cf2f9961cce313e97ab4c44b345df6de0371
asong57 <1998thddk@gmail.com>
2021-06-21 11:21:15 +0900

    버블 정렬

---
+++ 6. 정렬/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    //버블 정렬은 이웃한 두 요소의 대소 관계를 비교하여 교환을 반복한다.
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void bubbleSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (a[j - 1] > a[j]) {
+                    swap(a, j - 1, j);
+                }
+            }
+        }
+    }
+}
